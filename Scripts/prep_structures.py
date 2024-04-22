0a07f40c71cce009557f91fa8e3465ae6cafd8e7
Russell Davidson <rbdchemistry@gmail.com>
2024-04-21 21:50:36 -0400

    pushing fixes for handling the slightly different chromosome naming

--- Scripts/prep_structures.py
+++ Scripts/prep_structures.py
@@ -9,41 +9,38 @@ import pickle
 import sys
 
 af_dirs = list(Path(sys.argv[1]).glob('*/AlphaFold/'))
+outputDir = sys.argv[2]
 
 af_dirs.sort()
-
-outputDir = sys.argv[2]
 
 order = {}
 
 for af_dir in af_dirs:
-    structure_file = af_dir.glob('*min_00.pdb')
-    ranking_file = af_dir.glob('ranking_combined.json')
+    structure_file = list(af_dir.glob('*min_00.pdb'))[0]
+    ranking_file = list(af_dir.glob('ranking_combined.json'))[0]
     
     # parse directory string (e.g. Sphmag02G125400.1)
     try:
-        chromosomeID, geneID = re.findall('(\d+)',af_dir.parent.stem)
+        chromosomeID, geneID, temp = re.findall('(\d+)',af_dir.parent.stem)
     except:
         # unnumbered chromosomes are handled here
-        print(af_dir)
-        chromoseomID = 'U'
-        geneID = re.findall('(\d+)',af_dir.parent.stem)
-        #continue
+        chromosomeID = 'U'
+        geneID = re.findall('(\d+)',af_dir.parent.stem)[0]
 
     # get the subdirectory or make one
     chromosomeDict = order.get(chromosomeID,{})
     
-    with open(file, 'r') as json_in:
+    with open(ranking_file, 'r') as json_in:
         geneData = json.load(json_in)
         modelID  = geneData['order'][0]
         pTMs     = geneData['ptms'][modelID]
         pLDDTs   = geneData['plddts'][modelID]
 
     chromosomeDict[geneID] = [pTMs, pLDDTs]
-    
+
     order[chromosomeID] = chromosomeDict
 
-    shutil.move(ranking_file, outputDir + ranking_file.parent.parent.stem + '.json')
+    shutil.move(ranking_file, outputDir + 'ranking_jsons/' + ranking_file.parent.parent.stem + '.json')
 
     # load structure_file into MDA
     u = MDAnalysis.Universe(str(structure_file))
@@ -56,10 +53,10 @@ for af_dir in af_dirs:
     new_positions = positions @ Vt.T
     u_all.positions = new_positions
     # write centered, rotated structure to new directory
-    u_all.write(outputDir + structure_file.parent.parent.name + '.pdb')
+    u_all.write(outputDir + 'AlphaFold_structures/' + structure_file.parent.parent.name + '.pdb')
 
     print(f'Done working on {structure_file.parent.parent.name}')
 
-with open(outputDir + 'order.pkl','wb') as pkl_out:
+with open('order.pkl','wb') as pkl_out:
     pickle.dump(order, pkl_out)
 
